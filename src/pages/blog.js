07fa457f1e05393483c978cda2f1414f20056b26
KushBhatt96 <kush.bhatt96@yahoo.com>
2020-08-25 12:01:23 -0600

    Added a few gatsby plugins to use graphql to access markdown files in the project filesystem and display them to the screen

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,11 +1,40 @@
 import React from "react"
 import Layout from "../components/layout"
 import Theme from "../components/theme"
+import { graphql, useStaticQuery } from "gatsby"
 
 const Blog = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              title
+              date
+            }
+          }
+        }
+      }
+    }
+  `)
+
   return (
     <Layout>
-      <Theme title="Blog"></Theme>
+      <Theme title="Blog">
+        <div className="container">
+          <ol>
+            {data.allMarkdownRemark.edges.map(edge => {
+              return (
+                <li>
+                  <h2>{edge.node.frontmatter.title}</h2>
+                  <p>{edge.node.frontmatter.date}</p>
+                </li>
+              )
+            })}
+          </ol>
+        </div>
+      </Theme>
     </Layout>
   )
 }
